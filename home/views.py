659707899e59a01bbd06b492e24be9212b31e2e0
elch93 <eugeneliong@hotmail.com>
2020-05-21 13:14:03 +0800

    added login/register panel to home page

--- home/views.py
+++ home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
 def index(request):
     return render(request, 'home/index.template.html')
+
