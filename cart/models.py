71a1d9c231c2b85d0ce5940adea251ece9ad9cb3
elch93 <eugeneliong@hotmail.com>
2020-05-23 05:16:24 +0800

    fixed missing models.Model in Order model

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Order:
+class Order(models.Model):
     buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
