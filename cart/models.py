c30f9a607853e1a625ddcd2391b7c25c126241b2
elch93 <eugeneliong@hotmail.com>
2020-05-25 16:03:11 +0800

    added subtotal & delivery cost to order model

--- cart/models.py
+++ cart/models.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import User
 class Order(models.Model):
     buyer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     date = models.DateTimeField(auto_now_add=True)
+    subtotal = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=False, default=0)
+    delivery_cost = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=False, default=20.00)
     total = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
     summary = models.TextField(blank=True)
 
