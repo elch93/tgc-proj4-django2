d661d083ce1407dc77b5446e77ac174c2c469978
elch93 <eugeneliong@hotmail.com>
2020-05-26 14:31:47 +0800

    setup template for order transaction history

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 from django.contrib import messages
+from cart.models import Order
 # Create your views here.
 
 
@@ -11,10 +12,12 @@ def profile(request):
     if request.method == 'GET':
         profile = get_object_or_404(UserProfile, user=request.user)
         profile_form = UserProfileForm(instance=profile)
+        order_history = Order.objects.filter(buyer=request.user)
 
         context = {
             'profile': profile,
-            'profile_form': profile_form
+            'profile_form': profile_form,
+            'order_history': order_history
         }
         return render(request, 'profiles/profile.template.html', context)
 
@@ -23,6 +26,7 @@ def profile(request):
         updated_form = UserProfileForm(request.POST, instance=editing_profile)
         if updated_form.is_valid():
             updated_form.save()
-            messages.success(request, f'Profile for {request.user.username} has been updated.')
+            messages.success(
+                request, f'Profile for {request.user.username} has been updated.')
 
         return redirect('/profile')
