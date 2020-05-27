f4ead4335c16d2c3f430cdd4b49023d0d290c649
elch93 <eugeneliong@hotmail.com>
2020-05-27 13:30:08 +0800

    added mobile support for navbar

--- profiles/forms.py
+++ profiles/forms.py
@@ -6,4 +6,5 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ( 'contact', 'country', 'city', 'postal_code', 'street_address_1', 'street_address_2')
+        fields = ('contact', 'country', 'city', 'postal_code',
+                  'street_address_1', 'street_address_2')
